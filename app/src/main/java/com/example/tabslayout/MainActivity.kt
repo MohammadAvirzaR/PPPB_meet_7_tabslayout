1c93aec4db6a1e4fe0a50cea0d303adc58880269
Mohammad Avirza R <mohammadavirzaradyatanza@mail.ugm.ac.id>
2025-10-20 13:51:51 +0700

    init commit

---
+++ app/src/main/java/com/example/tabslayout/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tabslayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tabslayout.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val tabTitles = arrayOf("home","profile")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this
+        )
+        binding.viewPager.adapter = sectionsPagerAdapter
+
+
+        with(binding){
+
+
+        }
+    }
+}
